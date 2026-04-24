1aee705f392d6179af3b798b940d8d1c3e775a54
Peter <peter@narrowpacific.com>
2026-04-24 17:50:09 -0400

    Update from local

--- src/pages/PageMarketingEmails.jsx
+++ src/pages/PageMarketingEmails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Plus, Search, FileText, Send, Inbox, LayoutTemplate, X, ChevronRight, Trash2, MoreHorizontal, Clock, Edit2, Copy, Save, Check, Users } from "lucide-react";
-import { TanStackTable, PromptModal, DelModal, Modal } from "../components";
+import { TanStackTable, PromptModal, DelModal, Modal, ActBtns } from "../components";
 import { db, storage } from "../firebase";
 import { collection, addDoc, serverTimestamp, doc, updateDoc, deleteDoc, getDoc, query, where, getDocs, onSnapshot } from "firebase/firestore";
 import { ref, uploadBytes } from "firebase/storage";
